d5c9a93bb8f9eb8dcf858e63e0231bd07fa445b9
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-07 17:45:24 -0400

    adds auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dotodoamplify7eb84ca2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
